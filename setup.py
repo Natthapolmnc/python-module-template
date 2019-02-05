2172491bd499f2ca76d853942a672226184f4196
Fame <natthapol3011@gmail.com>
2019-02-05 20:45:13 +0700

    template

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='modulename',
+    version='1.0',
+    description='desciption of module',
+    long_description=readme(),
+    url='https://github.com/',
+    author='Fame',
+    author_email='natthapol.m@outlook.com',
+    license='',
+    install_requires=[
+    ],
+    scripts=['bin/****',
+            'bin/****',
+            'bin/****'],
+    keywords='',
+    packages=['modulename(packagename)'],
+    package_dir={'modulename(packagename)': 'src/__init__(Path)'},
+    package_data={'modulename' :['file'] #ใช้สำหรับเพิ่มไฟล์เข้าไปในแพ็กเกจเพราะบางไฟล์ ตอนบีบไฟล์ก่อนอัพขึ้นมันจะไม่ถูกรวมไปกับไฟล์บีบอัด
+    },
+)
